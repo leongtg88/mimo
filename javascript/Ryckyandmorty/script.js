e43aaeb368670a59b7e94ea93ff906c0ec6d9619
Leon <leongtg88@gmail.com>
2024-04-16 22:04:57 -0400

    se hace el primer fetch

--- javascript/Ryckyandmorty/script.js
+++ javascript/Ryckyandmorty/script.js
@@ -34,3 +34,23 @@ const data = {
           `;
     charactersContainer.appendChild(card);
   });
+
+
+/* en este codigo se hace nuestro primer FETCH donde se bajan los datos de una API  
+
+  const charactersContainer = document.getElementById("characters-container");
+  
+fetch("https://rickandmortyapi.com/api/character").then(response.json()).then(data =>{
+  data.results.forEach(results => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}" class="character-image">
+            <p class="character-detail"><span class="attribute">Name:</span> ${character.name}</p>
+            <p class="character-detail"><span class="attribute">Status:</span> ${character.status}</p>
+            <p class="character-detail"><span class="attribute">Species:</span> ${character.species}</p>
+        `;
+});
+
+  charactersContainer.appendChild(card);
+});
