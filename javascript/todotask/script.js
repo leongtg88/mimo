073f95f11f8cf071713c7e216b66a64a3356af65
Leon <leongtg88@gmail.com>
2024-05-03 21:36:12 -0400

    todotask, falta el delete task

---
+++ javascript/todotask/script.js
@@ -0,0 +1,28 @@
+function createTodo() {
+
+    const userInput = document.getElementById('note').value;
+  
+    const listItem = document.createElement("li");
+  
+    const todosContainerElement = document.getElementById('todos-container');
+    
+    listItem.innerHTML = userInput;
+    
+    todosContainerElement.appendChild(listItem);
+   
+    listItem.setAttribute("onclick", "todoCheck(event, this)");
+   
+    document.getElementById('note').value = '';
+  }
+  
+  function deleteTodos() {
+    
+  }
+  
+  function todoCheck(e, todo) {
+    if (todo.style.textDecoration === '') {
+      todo.style.textDecoration = 'line-through';
+    } else {
+      todo.style.textDecoration = '';
+    }
+  }
